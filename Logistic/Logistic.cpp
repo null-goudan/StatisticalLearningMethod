562f960f1baf3c473a678e408abca0ebd3b84d4d
goudan <837471654@qq.com>
2022-05-08 09:06:00 -0400

    Logustic

--- Logistic/Logistic.cpp
+++ Logistic/Logistic.cpp
@@ -5,12 +5,13 @@
 #include <cmath>
 #include <sstream>
 #include <fstream>
+#include <random>
 
 using namespace std;
 
 double sigmoid(double x);
 double scalarProduct(vector<double> w, vector<double> x, double bias = 0);
-void GradAscent(vector<vector<double>> DataSet_Feature, vector<double> Label, vector<double>& weight, double bias, double alpha);
+void GradAscent(vector<vector<double>> DataSet_Feature, vector<double> Label, vector<double>& weight, double alpha);
 vector<vector<double>> MatTransponse(vector<vector<double>> Mat);
 
 class Logistic{
@@ -20,19 +21,17 @@ class Logistic{
         Logistic(int feature_num, string filename){
             loadDataset(feature_num, filename);
             this->weight = vector<double>(feature_num);
-            this->bias = 0;
         }
-        void train(int iterNum, double lr = 0.003, void(*)(vector<vector<double>>, vector<double>, vector<double>&, double, double)=GradAscent);
+        void train(int iterNum, double lr = 0.003, void(*)(vector<vector<double>>, vector<double>, vector<double>&, double)=GradAscent);
         void loadDataset(int feature_num, string filename);
+        inline int pridict(vector<double> feature);
         inline vector<double> get_labels();
         inline vector<vector<double>> get_featuresMat();
         inline vector<double> get_weight();
-        inline double get_bias();
     private:
         vector<vector<double>> feature_Mat;
         vector<double> label_Mat;
         vector<double> weight;
-        double bias = 0;
 };
 
 vector<vector<double>> MatTransponse(vector<vector<double>> Mat){
@@ -58,10 +57,12 @@ void Logistic::loadDataset(int feature_num, string filename){
         string temp;
         int i = 0;
         while(getline(ss, temp, ' ')){
-            if(++i % feature_num == 0){
+            if(++i % (feature_num + 1) == 0){
                 y = stod(temp);
+                // cout<< y<<endl;
             }else{
                 x.push_back(stod(temp));
+                // cout<<stod(temp)<<" ";
             }
         }
         this->feature_Mat.push_back(x);
@@ -73,10 +74,6 @@ inline vector<double> Logistic::get_labels(){
     return this->label_Mat;
 }
 
-inline double Logistic::get_bias(){
-    return this->bias;
-}
-
 inline vector<vector<double>> Logistic::get_featuresMat(){
     return this->feature_Mat;
 }
@@ -85,27 +82,45 @@ inline vector<double> Logistic::get_weight(){
     return this->weight;
 }
 
-void Logistic::train(int iterNum, double lr, void(* train_method)(vector<vector<double>>, vector<double>, vector<double>&, double, double)){
+void Logistic::train(int iterNum, double lr, void(* train_method)(vector<vector<double>>, vector<double>, vector<double>&, double)){
     if(iterNum == 0) return ;
     if(this->feature_Mat.size()==0){printf("error! DataSet not load, Don't train!\n"); return ;}
     if(this->weight.size()==0){printf("Weight error! Don't train!\n"); return ;}
     while(iterNum--){
-        train_method(this->feature_Mat, this->label_Mat, this->weight, this->bias, lr);
+        train_method(this->feature_Mat, this->label_Mat, this->weight, lr);
+    }
+}
+
+inline int Logistic::pridict(vector<double> feature){
+    const int size = this->weight.size();
+    double res = 0.0;
+    for(int i =0;i<size;i++){
+        res +=  feature[i] * this->weight[i];
+    }
+    cout<< res<<" ";
+    res = sigmoid(res);
+    cout<<res<< " " ;
+    if(res>=0.5){
+        return 1;
+    }
+    else{
+        return 0;
     }
 }
 
 double sigmoid(double x){
-    return exp(x) / 1 + exp(x);
+    return exp(x) / (1 + exp(x));
 }
 
 double scalarProduct(vector<double> w, vector<double> x, double bias){
     double ret=0;
     for(int i=0;i<w.size();i++)
-        ret+=w[i]*x[i] + bias;
+        ret+=w[i]*x[i];
+    ret += bias;
     return ret;
 }
 
-void GradAscent(vector<vector<double>> DataSet_Feature, vector<double> Label, vector<double>& weight, double bias, double alpha){
+void GradAscent(vector<vector<double>> DataSet_Feature, vector<double> Label, vector<double>& weight, double alpha){
     vector<vector<double>> dataMatT=MatTransponse(DataSet_Feature);
     vector<double> h;
     vector<double> error;
@@ -113,8 +128,31 @@ void GradAscent(vector<vector<double>> DataSet_Feature, vector<double> Label, ve
         h.push_back(sigmoid(scalarProduct(data,weight)));
     for(int i=0;i<Label.size();i++)
         error.push_back(Label[i]-h[i]);
-    for(int i=0;i<weight.size();i++)
-        weight[i]+=alpha*scalarProduct(dataMatT[i],error);
+    for(int i=0;i<weight.size();i++){
+        weight[i] +=alpha*scalarProduct(dataMatT[i],error);
+    }
+}
+
+vector<vector<double>> random_datatest(){
+    vector<vector<double>> res;
+    default_random_engine e;
+    uniform_real_distribution<double> u_x1(0.0, 3.5);
+    uniform_real_distribution<double> u_x2(13.0, 20.5);
+    uniform_real_distribution<double> u_x1_2(0.0, 3.5);
+    uniform_real_distribution<double> u_x2_2(13.0, 20.5);
+    for(int i =0;i<30;i++){
+        vector<double> resp;
+        resp.push_back(u_x1(e));
+        resp.push_back(u_x2(e));
+        res.push_back(resp);
+    }
+    for(int i =0;i<30;i++){
+        vector<double> resp;
+        resp.push_back(u_x1_2(e));
+        resp.push_back(u_x2_2(e));
+        res.push_back(resp);
+    }
+    return res;
 }
 
 int main(){
@@ -122,12 +160,30 @@ int main(){
     vector<double> label;
     string filname("dataset.txt");
     Logistic L(2, filname);
-    L.train(500);
+    L.train(50000);
     vector<double> w;
     w = L.get_weight();
     for(auto i:w){
         cout<<i<<" ";
     }
-    cout<<L.get_bias();
+    // vector<double> sss;
+    // sss.push_back(5.2);
+    // sss.push_back(11.2);
+    // cout<<L.pridict(sss)<<endl;
+    vector<vector<double>> Feature_test;
+    ofstream f;
+    f.open("out.txt", ios::out);
+    Feature_test = random_datatest();
+    for(int i =0;i<Feature_test.size();++i){
+        for(auto j:Feature_test[i]){
+            f<<j<<" ";
+        }
+        f<<L.pridict(Feature_test[i])<<endl;
+    }
+    ofstream f1;
+    f1.open("weight.txt", ios::out);
+    for(auto i:w){
+        f1<<i<<" ";
+    }
     return 0;
 }
---
+++ Logistic/plot.py
@@ -0,0 +1,59 @@
+from tokenize import Double
+from matplotlib import widgets
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
+
+with open('./out.txt', 'r') as file:
+    lines = file.readlines()
+
+clusters = {}
+
+for line in lines:
+    split_line = line.split()
+    point = [float(x) for x in split_line[:-1]]
+    cluster_id = int(split_line[-1])
+    clusters.setdefault(cluster_id, []).append(point)
+
+
+clusters_ids = clusters.keys()
+cmap = cm.rainbow
+norm = Normalize(vmin=0, vmax=max(clusters_ids))
+
+for cluster_id in clusters_ids:
+
+    if cluster_id >= 0:
+        color = cmap(norm(cluster_id))
+        label = "cluster " + str(cluster_id)
+    elif cluster_id == -1:
+        color = "white"
+        label = "point isolé"
+    else:
+        color = "black"
+        label = "point non défini"
+
+    clusters[cluster_id] = (np.stack(clusters[cluster_id], 0), color, label)
+
+
+fig, ax = plt.subplots(figsize=(6,6))
+ax.set_xlabel('x')
+ax.set_ylabel('x2')
+ax.set_title('Exemple de clustering')
+
+for cluster_id, (points, color, label) in clusters.items():
+    ax.scatter(points[:,0], points[:,1], color=color, label=label, edgecolor="black")
+
+handles, labels = ax.get_legend_handles_labels() 
+handles, labels = zip(* sorted(zip(handles, labels), key=lambda t : t[1]))
+ax.legend(handles, labels)
+with open('./weight.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        ws = line.split()
+        weights = [float(x) for x in ws[0:-1]]
+        bias = float(ws[-1])
+x = np.arange(-3.0,3.0,0.1)
+y =  (-weights[0]+ weights[1]*x ) + bias
+ax.plot(x,y)
+plt.show()
